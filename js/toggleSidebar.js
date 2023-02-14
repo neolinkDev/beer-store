ff344d000e04911019d4bd03428c2e33b55c2e70
Jorge R <jorge@programmer.net>
2023-02-14 12:47:06 -0700

    fix sidebar scroll

--- js/toggleSidebar.js
+++ js/toggleSidebar.js
@@ -6,10 +6,15 @@ const $toggleNav      = getElement(".toggle-nav"),
       $sidebarOverlay = getElement(".sidebar-overlay"),
       $closeBtn       = getElement(".sidebar-close");
 
+const bHidden = getElement('body');
+
+
 $toggleNav.addEventListener('click', () => {
   $sidebarOverlay.classList.add('show');
+  bHidden.classList.add('b-hidden');
 });
 
 $closeBtn.addEventListener('click', () => {
   $sidebarOverlay.classList.remove('show');
+  bHidden.classList.remove('b-hidden');
 });
